775c05a86300f67060e739af0bdef6cf654e9a68
shakiba <shakiba.hamze70@gmail.com>
2023-11-29 17:52:29 +0330

    adding next-config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = (phase, { defaultConfig }) => {
+  if ("sassOptions" in defaultConfig) {
+    defaultConfig["sassOptions"] = {
+      prependData: `
+                      @import "./src/styles/_variables.scss";
+                      @import "./src/styles/_mixin.scss";
+                      `,
+    };
+  }
+  return { ...nextConfig };
+};
